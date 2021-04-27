5b360030ca6a5619608a544bea88481b6c371014
Stefan Peters <Stefan.Peters@gbv.de>
2021-04-27 09:18:00 +0200

    Update file structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import LoginClientVue from "."
-
-createApp(App).use(LoginClientVue).mount("#app")
+// import * as LoginClientVue from "."
+// createApp(App).use(LoginClientVue).mount("#app")
+import { Login, UserStatus } from "."
+createApp(App).use(Login).use(UserStatus).mount("#app")
